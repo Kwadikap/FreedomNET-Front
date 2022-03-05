b204f53b6c69600b198f3a61601776477cc7c1ef
Kwadwo <kwadikap@gmail.com>
2022-03-05 13:08:49 -0500

    socket io update

--- react-front-end/src/pages/messenger/Messenger.jsx
+++ react-front-end/src/pages/messenger/Messenger.jsx
@@ -20,7 +20,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://freedomnet-node-backend.herokuapp.com/api");
+    socket.current = io("/");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
