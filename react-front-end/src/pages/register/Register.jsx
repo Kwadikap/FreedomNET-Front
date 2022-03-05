3424002253a9d26c201b08fe47927e63ae33a3e5
Kwadwo <kwadikap@gmail.com>
2022-03-05 12:56:41 -0500

    first commit

---
+++ react-front-end/src/pages/register/Register.jsx
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import React, { useRef } from 'react';
+import './register.css'
+import { useNavigate, Link } from 'react-router-dom';
+
+
+const Register = () => {
+    const username = useRef();
+    const email = useRef();
+    const password = useRef();
+    const passwordAgain = useRef();
+    const navigate = useNavigate();
+
+    const handleClick = async (e) => {
+        e.preventDefault();   
+        if(passwordAgain.current.value !== password.current.value) {
+            passwordAgain.current.setCustomValidity("Passwords don't match!");
+        } 
+        else {
+            const user = {
+                username: username.current.value,
+                email: email.current.value,
+                password: password.current.value,
+            }
+            try {
+                await axios.post('https://freedomnet-node-backend.herokuapp.com/api/auth/register', user);
+                navigate('/login');
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    };
+
+    return (
+        <div className='login'>
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">FreedomNET</h3>
+                    <span className="loginDesc">Connect! Post! #SHARE</span>
+                </div>
+                <div className="loginRight">
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input 
+                            type="text" 
+                            required className="loginInput" 
+                            placeholder='Username' 
+                            ref={username} 
+                        />
+                        <input 
+                            type="email" 
+                            required 
+                            className="loginInput"
+                            placeholder='Email' 
+                            ref={email} 
+                        />
+                        <input 
+                            type="password" 
+                            required 
+                            className="loginInput" 
+                            placeholder='Password' 
+                            ref={password} 
+                            minLength='6'
+                        />
+                        <input 
+                            type="password" 
+                            required className="loginInput" 
+                            placeholder='Password Again' 
+                            ref={passwordAgain} 
+                        />
+                        <button className="loginButton" type='submit'>Sign Up</button>
+                        <button className="loginRegisterButton">
+                            <Link to='/login' style={{textDecoration: 'none', color:'white'}}>
+                                Log In 
+                            </Link>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
