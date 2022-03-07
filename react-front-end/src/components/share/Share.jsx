1d8716357c8fdf44ebdc90cb73c48116b8e77835
Kwadwo <kwadikap@gmail.com>
2022-03-06 20:27:00 -0500

    add more post extensions

--- react-front-end/src/components/share/Share.jsx
+++ react-front-end/src/components/share/Share.jsx
@@ -26,7 +26,6 @@ const Share = () => {
             data.append('name', fileName); 
             data.append('file', file); 
             newPost.img = fileName;
-            user.profilePicture = fileName;
             try {
                 await axios.post('https://freedomnet-node-backend.herokuapp.com/api/upload', data);
             } catch (err) {
