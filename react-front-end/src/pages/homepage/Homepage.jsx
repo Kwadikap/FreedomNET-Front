3896eb2e6e448225b43e8980ac3c679993aa163b
Kwadwo <kwadikap@gmail.com>
2022-03-07 19:11:48 -0500

    change leftbar to show friends

--- react-front-end/src/pages/homepage/Homepage.jsx
+++ react-front-end/src/pages/homepage/Homepage.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Homepage.css'
 import Topbar from '../../components/topbar/Topbar'
 import Leftbar from '../../components/leftbar/Leftbar.jsx';
 import Feed from '../../components/feed/Feed';
 import Rightbar from '../../components/rightbar/Rightbar'
+import { AuthContext } from '../../context/AuthContext'
+
+
 
 export default function Homepage() {
+  const { user } = useContext(AuthContext);
   return (
       <>
         <Topbar />
         <div className="homeContainer">
-        <Leftbar />
+        <Leftbar user={user} />
         <Feed />
         <Rightbar />
         </div>
